1afce2d7e294a44920f5488efe078c99e26444c2
farrinfedra <56314236+farrinfedra@users.noreply.github.com>
2020-09-18 19:00:33 +0300

    Updates

---
+++ precache-manifest.6ec76d61f187a3c649793b6bd4d5727f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba46a762a3a351864dc0af5900d30e1",
+    "url": "/pics-react/index.html"
+  },
+  {
+    "revision": "a86d11dabe5af7403fa4",
+    "url": "/pics-react/static/css/main.0c9a33c5.chunk.css"
+  },
+  {
+    "revision": "e8c3d88dfee25aefe0ba",
+    "url": "/pics-react/static/js/2.cb1fa45c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics-react/static/js/2.cb1fa45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86d11dabe5af7403fa4",
+    "url": "/pics-react/static/js/main.06458b61.chunk.js"
+  },
+  {
+    "revision": "54b0ffe07460e6db0543",
+    "url": "/pics-react/static/js/runtime-main.6695303f.js"
+  }
+]);
